cb5c719480b42dd8b1bbd0b1cf67763cef304072
Bibah-Bandhan <www.bibahbandhan@gmail.com>
2026-07-05 15:21:28 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+﻿const CACHE_NAME = "bibah-bandhan-v3";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./index.js",
+  "./bibah-bandhan-logo.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./bibah-bandhan-poster.jpg"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
