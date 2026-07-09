9fadd732cbf2b731beb3e058d2bcf81708026a54
Bibah-Bandhan <www.bibahbandhan@gmail.com>
2026-07-09 23:37:23 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
-﻿const CACHE_NAME = "bibah-bandhan-v3";
+﻿const CACHE_NAME = "bibah-bandhan-v4";
 
 const urlsToCache = [
   "./",
   "./index.html",
   "./index.js",
+  "./manifest.json",
   "./bibah-bandhan-logo.png",
   "./icon-192.png",
   "./icon-512.png",
@@ -14,13 +15,24 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys
+        .filter(key => key !== CACHE_NAME)
+        .map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
   );
 });
-
